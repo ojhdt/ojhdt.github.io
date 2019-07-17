0b9abe1ba8362ac32e290eb3f794482a44f347fc
ojhdt <ojhdtmail@gmail.com>
2019-07-17 08:27:55 +0800

    Site updated: 2019-07-17 08:27:54

--- js/warp.js
+++ js/warp.js
@@ -1,1 +1,1 @@
-$("img").not(".page__logo img").not(".post__cover img").not("heading.png").not("0.png").each(function(){var t=document.createElement("a");$(t).attr("data-fancybox","gallery"),$(t).attr("href",$(this).attr("src")),$(this).wrap(t)});
+$("img").not(".page__logo img").not(".post__cover img").not(".sidebar__img img").not(".footer__image img").each(function(){var t=document.createElement("a");$(t).attr("data-fancybox","gallery"),$(t).attr("href",$(this).attr("src")),$(this).wrap(t)});
